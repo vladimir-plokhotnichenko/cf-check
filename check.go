49f16670df4aa07526c5ba4442120ef87e9a2e6e
vladimir-plokhotnichenko <50494541+vladimir-plokhotnichenko@users.noreply.github.com>
2024-05-12 17:32:18 +0300

    Fixed .0 addresses
    Fixed .0 addresses (such as 188.114.96.0) being not detected as part of subnet

--- check.go
+++ check.go
@@ -3,21 +3,15 @@ package main
 import "net"
 
 func isCloudflare(ip net.IP) bool {
-	for _, c := range cidrs {
-		if c == "" {
-			continue
-		}
+        for _, c := range cidrs {
+                if c == "" {
+                        continue
+                }
 
-		hosts, err := hosts(c)
-		if err != nil {
-			continue
-		}
-
-		for _, host := range hosts {
-			if host == ip.String() {
-				return true
-			}
-		}
-	}
-	return false
+                _, cidrNet, _ := net.ParseCIDR(c)
+                if cidrNet.Contains(ip) {
+                        return true
+                }
+        }
+        return false
 }
