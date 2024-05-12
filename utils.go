b1ebaf8839455f196518d930c522a41124126f7e
vladimir-plokhotnichenko <50494541+vladimir-plokhotnichenko@users.noreply.github.com>
2024-05-12 17:55:08 +0300

    removed unused code

--- utils.go
+++ utils.go
@@ -14,35 +14,6 @@ func show(host string, ip net.IP, mode bool) {
 	}
 }
 
-func inc(ip net.IP) {
-	for j := len(ip) - 1; j >= 0; j-- {
-		ip[j]++
-		if ip[j] > 0 {
-			break
-		}
-	}
-}
-
-func hosts(cidr string) ([]string, error) {
-	ip, ipnet, err := net.ParseCIDR(cidr)
-	if err != nil {
-		return nil, err
-	}
-
-	var ips []string
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		ips = append(ips, ip.String())
-	}
-
-	lenIPs := len(ips)
-	switch {
-	case lenIPs < 2:
-		return ips, nil
-	default:
-		return ips[1 : len(ips)-1], nil
-	}
-}
-
 func isStdin() bool {
 	f, e := os.Stdin.Stat()
 	if e != nil {
